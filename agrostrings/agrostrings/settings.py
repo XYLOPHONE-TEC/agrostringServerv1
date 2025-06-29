d756ee77792a70b85dbac4d900ae2346690670c8
tracy <keeyatracy@gmail.com>
2025-06-29 20:17:06 +0300

    add media url to support image upload in development

--- agrostrings/agrostrings/settings.py
+++ agrostrings/agrostrings/settings.py
@@ -170,13 +170,20 @@ USE_TZ = True
 
 import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 #STATIC_URL = '/static/'
 
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
