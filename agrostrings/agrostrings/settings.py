8e7e2601b3b3c4e3e3a8b86e92518af66b5a933f
tracy <keeyatracy@gmail.com>
2025-07-01 23:56:38 +0300

    update allowed hosts to accept all domains

--- agrostrings/agrostrings/settings.py
+++ agrostrings/agrostrings/settings.py
@@ -39,7 +39,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
+ALLOWED_HOSTS = ["*"]
 
 
 AFRICASTALKING_USERNAME = os.getenv("AFRICASTALKING_USERNAME")
