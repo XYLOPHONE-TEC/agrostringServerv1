1aa1498e934e77524e54854f34b413530c183e12
tracy <keeyatracy@gmail.com>
2025-07-01 21:39:18 +0300

    add swagger to the endpoints

--- agrostrings/agrostrings/settings.py
+++ agrostrings/agrostrings/settings.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_yasg',
+
     #project apps
     'users',
     'farmers',
--- agrostrings/agrostrings/urls.py
+++ agrostrings/agrostrings/urls.py
@@ -21,14 +21,31 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Agrostrings API",
+      default_version='v1',
+      description="API documentation for Agrostrings backend",
+      contact=openapi.Contact(email="you@example.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/farmers/', include('farmers.urls')),
     path('api/buyers/', include('buyers.urls')),
     path('api/adminboard/', include('adminboard.urls')),
+
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 
 ]
@@ -36,3 +53,9 @@ urlpatterns = [
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
