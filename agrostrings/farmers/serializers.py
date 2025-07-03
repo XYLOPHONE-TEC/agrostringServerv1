d3ef5627bb6961ab42d8ef6c6b8e18cdbce133e0
tracy <keeyatracy@gmail.com>
2025-07-04 00:07:37 +0300

    include serializers for the admin reply, farmin put request,farmer community question,community reply

--- agrostrings/farmers/serializers.py
+++ agrostrings/farmers/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
-from .models import Produce  # CarbonTracker
+from .models import Produce, FarmerCommunityQuestion, CommunityReply, FarmInputRequest, AdminReplyToRequest
+
+
 
 
 class ProduceSerializer(serializers.ModelSerializer):
@@ -9,8 +11,36 @@ class ProduceSerializer(serializers.ModelSerializer):
         read_only_fields = ["farmer", "posted_at", "status"]
 
 
-# class CarbonTrackerSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = CarbonTracker
-#         fields = '__all__'
-#         read_only_fields = ['farmer', 'last_updated']
+
+
+class CommunityReplySerializer(serializers.ModelSerializer):
+    responder_name = serializers.CharField(source="responder.username", read_only=True)
+
+    class Meta:
+        model = CommunityReply
+        fields = '__all__'
+
+
+class FarmerCommunityQuestionSerializer(serializers.ModelSerializer):
+    replies = CommunityReplySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = FarmerCommunityQuestion
+        fields = '__all__'
+
+
+class AdminReplyToRequestSerializer(serializers.ModelSerializer):
+    admin_name = serializers.CharField(source="admin.username", read_only=True)
+
+    class Meta:
+        model = AdminReplyToRequest
+        fields = '__all__'
+
+
+class FarmInputRequestSerializer(serializers.ModelSerializer):
+    admin_replies = AdminReplyToRequestSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = FarmInputRequest
+        fields = '__all__'
+
