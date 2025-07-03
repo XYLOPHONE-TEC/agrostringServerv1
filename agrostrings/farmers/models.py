b8504ac589a7b3453536c540321d1b07b9e68acf
tracy <keeyatracy@gmail.com>
2025-07-03 23:24:08 +0300

    implement models for farmer community question and imputrequest

--- agrostrings/farmers/models.py
+++ agrostrings/farmers/models.py
@@ -29,44 +29,16 @@ class Produce(models.Model):
 
 
 
+class FarmerCommunityQuestion(models.Model):
+    farmer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
-# class CarbonActivity(models.Model):
-#     farmer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # or a FarmerProfile model
-#     uses_organic_fertilizer = models.BooleanField(default=False)
-#     uses_solar_energy = models.BooleanField(default=False)
-#     plants_trees = models.BooleanField(default=False)
-#     burns_waste = models.BooleanField(default=False)
-#     date_recorded = models.DateField(auto_now_add=True)
+class FarmInputRequest(models.Model):
+    farmer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    tool_name = models.CharField(max_length=100)
+    purpose = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_reviewed = models.BooleanField(default=False)
 
-#     def carbon_score(self):
-#         score = 0
-#         if self.uses_organic_fertilizer:
-#             score += 25
-#         if self.uses_solar_energy:
-#             score += 25
-#         if self.plants_trees:
-#             score += 25
-#         if not self.burns_waste:  # good if they don't burn
-#             score += 25
-#         return score
-
-#     def readiness_level(self):
-#         score = self.carbon_score()
-#         if score >= 75:
-#             return "High"
-#         elif score >= 50:
-#             return "Medium"
-#         else:
-#             return "Low"
-
-
-
-
-# class CarbonTracker(models.Model):
-#     farmer = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-#     total_planted_trees = models.IntegerField(default=0)
-#     sustainable_farming_score = models.FloatField(default=0.0)
-#     last_updated = models.DateTimeField(auto_now=True)
-
-#     def __str__(self):
-#         return f"Carbon Data for {self.farmer.username}"
