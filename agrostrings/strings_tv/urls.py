15ff13a75a26553b20b9804243a07112a1c77226
tracy <keeyatracy@gmail.com>
2025-07-08 09:29:40 +0300

    update strings and strings tv

---
+++ agrostrings/strings_tv/urls.py
@@ -0,0 +1,45 @@
+from django.urls import path
+from .views import (
+    VideoViewSet, CommentViewSet, VideoCategoryViewSet,
+    AgroStringsTVScheduleViewSet, RecommendedVideosView
+)
+
+# VIDEO paths
+video_list = VideoViewSet.as_view({'get': 'list', 'post': 'create'})
+video_detail = VideoViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
+video_like = VideoViewSet.as_view({'post': 'like'})
+video_view = VideoViewSet.as_view({'post': 'view'})
+
+# COMMENT paths
+comment_list = CommentViewSet.as_view({'get': 'list', 'post': 'create'})
+comment_detail = CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
+
+# CATEGORY paths
+category_list = VideoCategoryViewSet.as_view({'get': 'list', 'post': 'create'})
+category_detail = VideoCategoryViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
+
+# AGROSTRINGS TV paths
+tv_list = AgroStringsTVScheduleViewSet.as_view({'get': 'list', 'post': 'create'})
+tv_detail = AgroStringsTVScheduleViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update', 'delete': 'destroy'})
+
+# ROUTES
+urlpatterns = [
+    # VIDEO
+    path('videos/', video_list, name='video-list'),
+    path('videos/<int:pk>/', video_detail, name='video-detail'),
+    path('videos/<int:pk>/like/', video_like, name='video-like'),
+    path('videos/<int:pk>/view/', video_view, name='video-view'),
+    path('videos/recommended/', RecommendedVideosView.as_view(), name='video-recommended'),
+
+    # COMMENT
+    path('comments/', comment_list, name='comment-list'),
+    path('comments/<int:pk>/', comment_detail, name='comment-detail'),
+
+    # CATEGORY
+    path('categories/', category_list, name='category-list'),
+    path('categories/<int:pk>/', category_detail, name='category-detail'),
+
+    # AGROSTRINGS TV
+    path('tv/', tv_list, name='tv-list'),
+    path('tv/<int:pk>/', tv_detail, name='tv-detail'),
+]
